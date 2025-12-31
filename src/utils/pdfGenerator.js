fa75084fa0318064a0270c381ef8783a4bf3b755
rojamuthiah <romu0304@gmail.com>
2025-12-31 13:14:30 +0530

    pdf geneartor change

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -3,7 +3,10 @@ const puppeteer = require("puppeteer");
 async function generatePDF(html) {
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--no-sandbox"]
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox"
+    ]
   });
 
   const page = await browser.newPage();
